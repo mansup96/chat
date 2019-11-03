edcf4c2d102b9b0d754030d12e50bfed9e196fc6
artem taranyuk <poquepoque27@gmail.com>
2019-11-04 02:01:47 +0500

    #6 minor fixes

--- server/proxy.js
+++ server/proxy.js
@@ -7,4 +7,7 @@ const log = console.log;
 
 app.use("/", proxy(`http://localhost:${process.env.EXPRESS_PORT}`));
 
-app.listen(process.env.PROXY_PORT, () => log(chalk.blue.bold(`Proxy is active on port ${process.env.PROXY_PORT}`)));
+app.listen(process.env.PROXY_PORT, () => {
+  log(chalk.blue.bold(`Proxy is active on port ${process.env.PROXY_PORT}`));
+  log(chalk.yellowBright.bold(`Ready to serve http://localhost:${process.env.PROXY_PORT}`));
+});
