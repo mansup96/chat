0b54b864e7071d6b5e4e346fcdaab7238a78a733
artem taranyuk <poquepoque27@gmail.com>
2019-11-07 21:39:00 +0500

    #14 added mongodb support. Not finished, has bugs

--- server/chat.js
+++ server/chat.js
@@ -1,188 +1,272 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const socket = require("socket.io");
-const express = require("express");
-const http = require("http");
-const middleware = require("socketio-wildcard")();
-const multer = require("multer");
-const cors = require("cors");
-const path = require("path");
-const chalk = require("chalk");
-const fs = require('fs');
+const socket = require('socket.io')
+const express = require('express')
+const http = require('http')
+const middleware = require('socketio-wildcard')()
+const multer = require('multer')
+const cors = require('cors')
+const path = require('path')
+const chalk = require('chalk')
+const fs = require('fs')
+const mongoose = require('mongoose')
+const { MessageSchema, UserSchema } = require('./schemas')
 
-const staticFilesRoute = "/static";
-const staticFilesDir = __dirname + "/uploads";
+const staticFilesRoute = '/static'
+const staticFilesDir = __dirname + '/uploads'
 
-const expressPort = process.env.EXPRESS_PORT;
+const expressPort = process.env.EXPRESS_PORT
+const mongoPort = process.env.MONGO_PORT
 
-if (!fs.existsSync(staticFilesDir)){
-    fs.mkdirSync(staticFilesDir);
+if (!fs.existsSync(staticFilesDir)) {
+  fs.mkdirSync(staticFilesDir)
 }
 
 const storageConfig = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, staticFilesDir);
+    cb(null, staticFilesDir)
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}${file.originalname}`);
-  }
-});
+    cb(null, `${Date.now()}${file.originalname}`)
+  },
+})
 
-var upload = multer({ storage: storageConfig, });
+var upload = multer({ storage: storageConfig })
 
-const app = express();
-const server = http.Server(app);
-const io = socket(server);
+const app = express()
+const server = http.Server(app)
+const io = socket(server)
 
-io.use(middleware);
-app.use(cors());
-app.use(staticFilesRoute, express.static(staticFilesDir));
+console.log(chalk.gray('Connecting to db'))
+mongoose
+  .connect(`mongodb://localhost:${mongoPort}/chat`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 203,
+  })
+  .then(ere => {
+    console.log(chalk.green('Connected to db'))
+  })
+  .catch(error => {
+    console.log(error)
 
-app.use("/", express.static(path.join(__dirname, "..", "client")));
+    process.exit(-1)
+  })
 
-server.listen(expressPort, () => console.log(chalk.blue.bold(`Express is active on ${expressPort}`)));
+io.use(middleware)
+app.use(cors())
+app.use(staticFilesRoute, express.static(staticFilesDir))
+
+app.use('/', express.static(path.join(__dirname, '..', 'client')))
+
+server.listen(expressPort, () =>
+  console.log(chalk.blue.bold(`Express is active on ${expressPort}`))
+)
 
 function getImageUrl(imageName) {
-  return `${staticFilesRoute}/${imageName}`;
+  return `${staticFilesRoute}/${imageName}`
 }
 
 app.post(
-  "/photos/upload",
-  upload.fields([{ name: "image", maxCount: "1" }]),
+  '/photos/upload',
+  upload.fields([{ name: 'image', maxCount: '1' }]),
   function(req, res, next) {
     if (req.files && req.files.image && req.files.image[0]) {
-      const imageUrl = getImageUrl(req.files.image[0].filename);
-      res.send({ imageUrl });
+      const imageUrl = getImageUrl(req.files.image[0].filename)
+      res.send({ imageUrl })
 
-      console.log(imageUrl);
+      console.log(imageUrl)
     } else {
-      res.sendStatus(403);
+      res.sendStatus(403)
     }
     // req.body will contain the text fields, if there were any
   }
-);
+)
 
 const clientCommands = {
-  setName: "set-name",
-  sendMessage: "send-message",
-  sendHandshake: "send-handshake"
-};
+  setName: 'set-name',
+  sendMessage: 'send-message',
+  sendHandshake: 'send-handshake',
+}
 
 const serverCommands = {
-  userSetName: "user-set-name",
-  sendSystemMessage: "send-system-message",
-  sendClientMessage: "send-client-message",
-  error: "server-error",
-  sendMessagesHistory: "send-messages-history",
-  setHandshake: "set-handshake"
-};
+  userSetName: 'user-set-name',
+  sendSystemMessage: 'send-system-message',
+  sendClientMessage: 'send-client-message',
+  error: 'server-error',
+  sendMessagesHistory: 'send-messages-history',
+  setHandshake: 'set-handshake',
+}
 
 const errors = {
-  NO_HANDSHAKE: "NO_HANDSHAKE"
-};
+  NO_HANDSHAKE: 'NO_HANDSHAKE',
+}
 
-const messages = [];
+const messages = []
 
-let users = {};
+let users = {}
 
-io.on("connect", handleConnection);
+io.on('connect', handleConnection)
 
-function addMessage(userId, { text, imageUrl }) {
-  const timestamp = Date.now();
+function createMessage({ userId, text, imageUrl, isSystem }) {
+  const timestamp = Date.now()
 
-  const message = {
-    userId,
+  const message = new MessageSchema({
+    _id: new mongoose.Types.ObjectId(),
+    user: userId,
     message: text,
+    isSystem,
     imageUrl,
-    timestamp
-  };
+    timestamp,
+  })
 
-  messages.push(message);
+  return message
+}
 
-  return message;
+function addMessage({ userId, text, imageUrl, isSystem = false }) {
+  return new Promise((resolve, reject) => {
+    createMessage({ userId, text, isSystem, imageUrl }).save(
+      (err, document) => {
+        if (err) reject(err)
+
+        resolve(document)
+      }
+    )
+  })
 }
 
 function createUser(name) {
   return {
-    name
-  };
-}
-
-function addUser(id) {
-  users = { ...users, [id]: createUser(`name_${id}`) };
-}
-
-function setName(id, name) {
-  if (users[id]) {
-    users[id].name = name;
+    name,
   }
+}
+
+function addUser(_id) {
+  const user = new UserSchema({ _id })
+  user.save()
+}
+
+function setName(_id, name) {
+  UserSchema.findOne({ _id }, (err, res) => {
+    res.name = name
+
+    res.save()
+  })
 }
 
 function handleHandshake(handshake) {
   if (!handshake) {
-    addUser(this.id);
+    addUser(this.id)
 
-    this.emit(serverCommands.setHandshake, this.id);
+    this.emit(serverCommands.setHandshake, this.id)
 
-    this.handshakeid = this.id;
+    this.handshakeid = this.id
   } else {
-    this.handshakeid = handshake;
+    this.handshakeid = handshake
     if (!users[handshake]) {
-      addUser(handshake);
+      addUser(handshake)
     }
   }
 
-  this.emit(serverCommands.sendMessagesHistory, messages);
+  this.emit(serverCommands.sendMessagesHistory, messages)
   io.emit(
     serverCommands.sendSystemMessage,
     `${users[this.handshakeid].name} has connected`
-  );
+  )
+}
+
+async function sendSystemMessageToAll(msg, save = false) {
+  const message = createMessage(null, { text: 'fsds' }, true)
+
+  io.emit(serverCommands.sendClientMessage, message)
 }
 
 function handleSetName(name) {
-  const { handshakeid } = this;
+  const { handshakeid } = this
 
   if (!this.handshakeid) {
-    this.emit(errors.NO_HANDSHAKE);
+    this.emit(errors.NO_HANDSHAKE)
 
-    return;
+    return
   }
 
-  const prevName = users[handshakeid].name;
-
-  setName(handshakeid);
-  io.emit(serverCommands.userSetName, (handshakeid, name));
-  io.emit(
-    serverCommands.sendSystemMessage,
-    `User ${prevName} has set name to ${name}`
-  );
+  UserSchema.findOne({ _id: handshakeid }, (err, prevUser) => {
+    setName(handshakeid, name)
+    io.emit(serverCommands.userSetName, { id: handshakeid, name })
+    sendSystemMessageToAll(
+      `${prevUser.name || prevUser.id} changed name to ${name}`
+    )
+  })
 }
 
-function handleSendMessage({ text, imageUrl }) {
-  const { handshakeid } = this;
+async function handleHandshake(handshake) {
+  const userId = new mongoose.Types.ObjectId()
 
-  if (!this.handshakeid) {
-    this.emit(serverCommands.error, errors.NO_HANDSHAKE);
+  if (!handshake) {
+    addUser(userId)
 
-    return;
+    this.emit(serverCommands.setHandshake, userId)
+
+    this.handshakeid = userId
+  } else {
+    this.handshakeid = handshake
+
+    await UserSchema.findOne({ _id: handshake }, (err, user) => {
+      if (!user) {
+        addUser(userId)
+        this.emit(serverCommands.setHandshake, userId)
+      }
+    })
   }
 
-  const message = addMessage(handshakeid, { text, imageUrl });
+  MessageSchema.find()
+    .populate('user')
+    .exec((err, res) => {
+      console.log(res)
+      this.emit(serverCommands.sendMessagesHistory, res)
+    })
 
-  io.emit(serverCommands.sendClientMessage, message);
+  UserSchema.findOne({ _id: handshake }, (err, result) => {
+    let name = handshake
+
+    if (result) {
+      name = result.name || result._id
+    }
+
+    sendSystemMessageToAll(`${name} has connected`)
+  })
+}
+
+async function handleSendMessage({ text, imageUrl }) {
+  const { handshakeid } = this
+
+  if (!this.handshakeid) {
+    this.emit(serverCommands.error, errors.NO_HANDSHAKE)
+
+    return
+  }
+
+  if (!text) return
+
+  const message = await addMessage({ userId: handshakeid, text, imageUrl })
+
+  MessageSchema.findById(message._id)
+    .populate('user')
+    .exec((err, result) => {
+      io.emit(serverCommands.sendClientMessage, result)
+    })
 }
 
 function handleConnection(connection) {
-  const onHandshake = handleHandshake.bind(connection);
-  const onSetName = handleSetName.bind(connection);
-  const onSendMessage = handleSendMessage.bind(connection);
+  const onHandshake = handleHandshake.bind(connection)
+  const onSetName = handleSetName.bind(connection)
+  const onSendMessage = handleSendMessage.bind(connection)
 
-  connection.on(clientCommands.sendHandshake, onHandshake);
+  connection.on(clientCommands.sendHandshake, onHandshake)
 
-  connection.on("*", console.log);
+  connection.on('*', console.log)
 
-  connection.on(clientCommands.setName, onSetName);
+  connection.on(clientCommands.setName, onSetName)
 
-  connection.on(clientCommands.sendMessage, onSendMessage);
+  connection.on(clientCommands.sendMessage, onSendMessage)
 }
-
