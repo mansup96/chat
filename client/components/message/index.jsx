18617cae19799b90877541d46ef93e6a2392869d
Volodya!! <you@example.com>
2019-11-07 19:06:37 +0500

    #13 added css loader

--- client/components/message/index.jsx
+++ client/components/message/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import style from './style.css'
 
 const isMyMessage = ID => ID === localStorage.getItem('ID')
 
@@ -31,6 +32,9 @@ class Message extends React.PureComponent {
 
     return (
       <div className={msgContClass}>
+        <div className={style.huita}>
+          fa
+        </div>
         <div className={msgclass}>
           <div className="name">{userId}</div>
           {imageUrl ? (
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       {
         test: /\.css/,
         loader:
-          'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'style-loader!css-loader?modules&importLoaders=true&localIdentName=[local]___[hash:base64:5]',
       },
       {
         test: /\.(jpg|png)/,
